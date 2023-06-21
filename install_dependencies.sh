2a761783bc97bb192df816b509d79429a77b38cd
dhi13man <furyx.ds@gmail.com>
2023-06-22 00:56:53 +0530

    Finalise deployment lifecycle

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 cd $(dirname $0)
+curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash - &&\
+yum install -y nodejs
+npm install -g forever
+npm install -g pm2
 npm install >> ~/last_install_dependencies.log
--- start_server.sh
+++ start_server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd $(dirname $0)
-npm run start
+pm2 start npm -- start
