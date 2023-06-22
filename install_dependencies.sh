18b31e26d60c29fca4ccb5a21f8549c399653f1b
dhi13man <furyx.ds@gmail.com>
2023-06-22 20:02:14 +0530

    AWS Parameter store env export

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -4,7 +4,7 @@ source ~/.bashrc
 cd $(dirname $0)
 
 # Add environment variables to a .env file
-export MONGO_URL="$(aws ssm get-parameter --name MONGO_URI --query "Parameter.Value" --output text)"
+export MONGO_URI="$(aws ssm get-parameter --name MONGO_URI --query "Parameter.Value" --output text)"
 export JWT_SECRET_KEY="$(aws ssm get-parameter --name JWT_SECRET_KEY --query "Parameter.Value" --output text)"
 
 # Clean up
