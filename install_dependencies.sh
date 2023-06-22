2663620e7447bfae7c9d1021b303ddc06dd217cc
dhi13man <furyx.ds@gmail.com>
2023-06-22 18:46:22 +0530

    create env variable file from shell env variables

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -2,13 +2,17 @@
 source ~/.bashrc
 
 cd $(dirname $0)
-curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash - &&\
-yum install -y nodejs
-npm install -g forever
-npm install -g pm2
-npm install >> ~/last_install_dependencies.log
 
 # Add environment variables to a .env file
 rm -f .env
 echo "MONGO_URI=$MONGO_URI" >> .env
 echo "JWT_SECRET_KEY=$JWT_SECRET_KEY" >> .env
+
+# Node JS Install
+curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash - &&\
+yum install -y nodejs
+
+# Install dependencies
+npm install -g forever
+npm install -g pm2
+npm install
