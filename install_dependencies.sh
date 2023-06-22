522ececdf59e243ad420476e52356b981e0f359c
dhi13man <furyx.ds@gmail.com>
2023-06-22 19:06:55 +0530

    Node magic

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -8,12 +8,18 @@ rm -f ./.env
 echo "MONGO_URI=$MONGO_URI" >> ./.env
 echo "JWT_SECRET_KEY=$JWT_SECRET_KEY" >> ./.env
 
+# Clean up
+rm -rf /usr/local/bin/npm /usr/local/share/man/man1/node* ~/.npm
+rm -rf /usr/local/lib/node*
+rm -rf /usr/local/bin/node*
+rm -rf /usr/local/include/node*
+yum purge nodejs npm
+yum autoremove
+rm -rf ./node_modules
+
 # Node JS Install
 curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash - &&\
 yum install -y nodejs
-
-# Clean up
-rm -rf ./node_modules
 
 # Install dependencies
 npm install -g forever
