d884fbe3542887a222cba08690fb55633d2eed05
dhi13man <furyx.ds@gmail.com>
2023-06-24 12:10:56 +0530

    ES lint

--- app.js
+++ app.js
@@ -10,13 +10,13 @@ app.use(cors());
 app.use(require('./routers/agrijod-actuator'));
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
 
 app.use('/seller', require('./routers/sellerRoute'));
 app.use('/item', require('./routers/generalListing'));
 app.use('/buyer', require('./routers/buyerRoute'));
 app.use('/add-to-cart', require('./routers/addToCart'));
-app.use('/transporter', require('./routers/transportRoute'))
+app.use('/transporter', require('./routers/transportRoute'));
 
 module.exports = app;
