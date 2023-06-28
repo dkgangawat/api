851858d1e80454d7c35b53882180a008000b5a9f
deepak <bairwaddeepak@gmail.com>
2023-06-29 00:02:33 +0530

    .

--- config/config.js
+++ config/config.js
@@ -3,6 +3,6 @@ require('dotenv').config();
 let PORT = process.env.PORT || 8000;
 let JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
 let MONGO_URI = process.env.MONGO_URI;
-let GOOGLE_MAP_KEY = process.env.GOOGLE_MAP_KEY
+let GOOGLE_MAP_KEY = "AIzaSy88621sdOZY-lEwqTJcCeWHSTdZ0eypqdfY"
 
 module.exports = { PORT, JWT_SECRET_KEY, MONGO_URI, GOOGLE_MAP_KEY };
